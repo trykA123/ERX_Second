a2288c458fb3ebe235d10a16269b45d9fbd81c1f
trykA <claudiu.tancau08@gmail.com>
2023-09-18 17:20:37 +0300

    Dada

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,7 +4,15 @@ import ServicesCard from "../components/ServicesCard";
 const Services = () => {
   return (
     <div id="work" className="container mx-auto h-full">
-      <div className="xl:py-36 "></div>
+      <div className="xl:py-36 flex flex-col space-y-16 px-4">
+        <h2 className="text-8xl">Services</h2>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora
+          nostrum quo officiis, rem, veritatis pariatur repellendus at cum
+          labore ea nobis sint earum et minus! Quaerat beatae molestias
+          exercitationem.
+        </p>
+      </div>
       <div className="xl:py-12 grid grid-cols-3 content-center">
         <ServicesCard />
         <ServicesCard />
